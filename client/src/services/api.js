b62c7714c00fe6ea112bb5ece2cd0bf3f8371152
patel-nip <patelnip09@gmail.com>
2026-02-05 00:20:38 +0530

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://glido-65i8.vercel.app/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
